b55656e0c8d03960b2b16b83e94a479528c0b0b7
ImJustNon <Im_Just_Non@hotmail.com>
2023-07-23 23:33:36 +0700

    try to add again

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const urlencoded = bodyparser.urlencoded({
 });
 const config = require("./configs/config.js");
 
-
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust the '*' to restrict access to specific origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 server.use(cors());
 // server.use((req,res,next)=>{
 //     res.setHeader('Access-Control-Allow-Headers', '*');
