db9c4a9bd4a83b8c2c36c7fca17e595fe5c7420a
ImJustNon <Im_Just_Non@hotmail.com>
2023-08-09 13:33:03 +0700

    add swagger ui

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const axios = require("axios");
 const useragent = require('express-useragent');
 const mysql = require("mysql2");
 const bodyparser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSdoc = require("swagger-jsdoc");
 // const session = require('express-session');
 
 const urlencoded = bodyparser.urlencoded({
@@ -52,6 +54,37 @@ fs.readdirSync("./routes").forEach(async files => {
         console.log('[Router] ' + `Fail to Load : ` + files);
     }
 });
+
+// swagger definition
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "SBTVC Dormitory Access System 2.0 API Document",
+            version: "10.2.3",
+            description: "API Documentation"
+        },
+        servers: [
+            {
+                url: "https://sbtvc-das-api.nonlnwza.xyz",
+            },
+        ],
+        path: [
+            {
+                get: {
+                    description: "asdasd"
+                }
+            }
+        ]
+    },
+    apis: [
+        'server.js',
+    ],
+}
+
+const swaggerSpec = swaggerJSdoc(swaggerOptions);
+server.use('/document', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 // database
 require("./database/connect.js").connect();
