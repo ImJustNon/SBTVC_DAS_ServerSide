28d5d609ad55944760383f4226e292ae0c8a96f3
ImJustNon <Im_Just_Non@hotmail.com>
2023-08-19 17:55:33 +0700

    add true false option

--- routes/admin_update_allow.js
+++ routes/admin_update_allow.js
@@ -11,7 +11,7 @@ const urlEncoded = bodyparser.urlencoded({
 
 
 router.post('/api/admin/form/update_allow', async(req, res) =>{
-    const { student_id } = req.body ?? {};
+    const { student_id, update_status } = req.body ?? {};
 
     if(!student_id){
         return res.json({
@@ -20,22 +20,49 @@ router.post('/api/admin/form/update_allow', async(req, res) =>{
         });
     }
 
-    connection.execute("UPDATE send_form_date SET allow=? WHERE student_id=?", ["true", student_id], async(error, results, fields) =>{
-        if(error){
-            return res.json({
-                status: "FAIL",
-                error: `Mysql error :: ${error}`
-            });
-        }
-
-        return res.json({
-            status: "SUCCESS",
-            error: null,
-            data: {
-                results: results,
+    if(update_status.toUpperCase() == "true"){
+        connection.execute("UPDATE send_form_date SET allow=? WHERE student_id=?", ["true", student_id], async(error, results, fields) =>{
+            if(error){
+                return res.json({
+                    status: "FAIL",
+                    error: `Mysql error :: ${error}`
+                });
             }
+    
+            return res.json({
+                status: "SUCCESS",
+                error: null,
+                data: {
+                    results: results,
+                }
+            });
         });
-    });
+    }
+    else if(update_status.toUpperCase() == "false") {
+        connection.execute("UPDATE send_form_date SET allow=? WHERE student_id=?", ["false", student_id], async(error, results, fields) =>{
+            if(error){
+                return res.json({
+                    status: "FAIL",
+                    error: `Mysql error :: ${error}`
+                });
+            }
+    
+            return res.json({
+                status: "SUCCESS",
+                error: null,
+                data: {
+                    results: results,
+                }
+            });
+        });
+    }
+    else {
+        return res.json({
+            status: "FAIL",
+            error: "Invalid update_status option",
+        });
+    }
+    
 });
 
 module.exports = router;
