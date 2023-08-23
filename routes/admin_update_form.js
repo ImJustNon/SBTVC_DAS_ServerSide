1169e62b9eb1a1d1d76d2d18e26a5d1af6a09d72
ImJustNon <Im_Just_Non@hotmail.com>
2023-08-23 21:57:26 +0700

    add updatedate form api for admin page

--- routes/admin_update_form.js
+++ routes/admin_update_form.js
@@ -10,47 +10,75 @@ const urlEncoded = bodyparser.urlencoded({
 });
 
 
-router.post('/api/admin/form/update_form/update_allow', async(req, res) =>{
-    const { update_allow, student_id } = req.body ?? {};
+router.post('/api/admin/form/update_form/update_out_location_auth', urlEncoded, async(req, res) =>{
+    const { student_id, status } = req.body ?? {};
 
-    if(!update_allow){
+    if(!student_id || !status){
         return res.json({
             status: "FAIL",
-            error: "Cant find update_allow from body"
+            error: `Missing student_id or status`,
         });
     }
 
-    if(update_allow.toLowerCase() === "true"){
-        connection.execute(`UPDATE send_form_table SET allow='true' WHERE student_id=?`, [student_id], async(error, results, fields) =>{
-            if(error){
-                return res.json({
-                    status: "FAIL",
-                    error: `Mysql error :: ${error}`
-                });
-            }
+    if(status !== 'true' || status !== 'false'){
+        return res.json({
+            status: "FAIL",
+            error: `invalid status option`,
+        });
+    }
 
+    connection.execute(`UPDATE send_form_table SET out_location_auth=? WHERE student_id=?`, [status.toLowerCase(), student_id], async(error, results, fields) =>{
+        if(error){
             return res.json({
-                status: "SUCCESS",
-                error: null,
-                data: {
-                    student_id: student_id,
-                }
+                status: "FAIL",
+                error: `Mysql error :: ${error}`,
             });
+        }
+
+        return res.json({
+            status: "SUCCESS",
+            error: null,
+            data: {
+                results: results,
+            },
+        });
+    });
+});
+
+router.post('/api/admin/form/update_form/update_in_location_auth', urlEncoded, async(req, res) =>{
+    const { student_id, status } = req.body ?? {};
+
+    if(!student_id || !status){
+        return res.json({
+            status: "FAIL",
+            error: `Missing student_id or status`,
         });
     }
 
-    if(update_allow.toLowerCase() === "false"){
-        connection.execute(`UPDATE send_form_table SET allow='false' WHERE student_id=?`, [student_id], async(error, results, fields) =>{
-            if(error){
-                return res.json({
-                    status: "FAIL",
-                    error: null,
-                });
-            }
+    if(status !== 'true' || status !== 'false'){
+        return res.json({
+            status: "FAIL",
+            error: `invalid status option`,
         });
-    }   
-    
+    }
 
+    connection.execute(`UPDATE send_form_table SET in_location_auth=? WHERE student_id=?`, [status.toLowerCase(), student_id], async(error, results, fields) =>{
+        if(error){
+            return res.json({
+                status: "FAIL",
+                error: `Mysql error :: ${error}`,
+            });
+        }
+
+        return res.json({
+            status: "SUCCESS",
+            error: null,
+            data: {
+                results: results,
+            },
+        });
+    });
 });
+
 
 module.exports = router;
